cdc2ef2a37963911fa3e811d1ba8ae0f118daacf
PittnoiR <omargarza394@gmail.com>
2024-08-12 13:23:58 -0600

    comenzar chat  con socket io y api creada

--- backend/api.js
+++ backend/api.js
@@ -1,20 +1,29 @@
+
+
+//API requires ------------------------------------
 const express = require('express');
 const crypto = require('crypto');
 const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
 
-const file= path.join(__dirname,'data.json')
+//socket.io requires-----------------------------------------------------
 
+const  logger = require('morgan');
+const  {Server} = require('socket.io');
+const {createServer} = require('http') 
+
+
+//API de comucicacion con el front------------------
+const file= path.join(__dirname,'data.json')
 const data = fs.readFileSync(file);
 const jsonData = JSON.parse(data);
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+app.use(logger('dev'));
 
 app.use(cors());
 app.use(express.json());
-
 /*
 app.get('/users',(req,res)=>{
     res.header('Access-Control-Allow-Origin', '*');
@@ -24,8 +33,6 @@ app.get('/users',(req,res)=>{
     res.status(200).json(jsonData);
 })
 */
-
-
 app.post('/users/new',(req,res)=>{  
     const {userName, email, password} = req.body;
     const userExist = jsonData.find(user => {
@@ -69,6 +76,19 @@ app.post('/users',(req,res)=>{
         return res.status(203).json({msg:"user found"});
     };
 });
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
+
+
+//socket.io--------------------------------------------------------------
+const PORTsocket = process.env.PORT || 42066;
+const socketServer = createServer(app);
+
+const io  = new Server(socketServer)
+io.on('connection',(socket)=>{
+    console.log("user connected")
 })
+
+socketServer.listen(PORTsocket,()=>{
+    console.log("server socket and API running on:",PORTsocket)
+})
+
+
